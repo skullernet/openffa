f5d5276b7c1567ed01bd61f278294ab5041b8b9d
Andrey Nazarov <skuller@skuller.net>
2007-09-23 23:58:45 +0000

    Replaced link_t stuff with list_t.

--- g_local.h
+++ g_local.h
@@ -581,10 +581,8 @@ extern	int	meansOfDeath;
 
 extern	edict_t			*g_edicts;
 
-#define	FOFS(x) (int)&(((edict_t *)0)->x)
-#define	STOFS(x) (int)&(((spawn_temp_t *)0)->x)
-#define	LLOFS(x) (int)&(((level_locals_t *)0)->x)
-#define	CLOFS(x) (int)&(((gclient_t *)0)->x)
+#define	FOFS(x) q_offsetof( edict_t, x )
+#define	STOFS(x) q_offsetof( spawn_temp_t, x )
 
 #define random()	((rand () & 0x7fff) / ((float)0x7fff))
 #define crandom()	(2.0 * (random() - 0.5))
@@ -1067,7 +1065,7 @@ struct edict_s
 	int			linkcount;
 
 	// FIXME: move these fields to a server private sv_entity_t
-	link_t		area;				// linked to a division node or leaf
+	list_t		area;				// linked to a division node or leaf
 	
 	int			num_clusters;		// if -1, use headnode instead
 	int			clusternums[MAX_ENT_CLUSTERS];
