11933dd83c8b73e7eb1983246acdb1da37575de7
Andrey Nazarov <skuller@skuller.net>
2011-05-13 14:25:31 +0400

    Reset modelindex4 properly.

--- p_hud.c
+++ p_hud.c
@@ -313,7 +313,7 @@ void MoveClientToIntermission (edict_t *ent)
     ent->s.modelindex = 0;
     ent->s.modelindex2 = 0;
     ent->s.modelindex3 = 0;
-    ent->s.modelindex = 0;
+    ent->s.modelindex4 = 0;
     ent->s.effects = 0;
     ent->s.sound = 0;
     ent->s.event = 0;
